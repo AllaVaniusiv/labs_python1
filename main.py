704e0ed4aaede93efac0244f6bece4bbeeeccd32
AllaVaniusiv <allavaniusiv@gmail.com>
2023-05-26 13:28:18 +0300

    adding lab 2 with fixes

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from managers.Manager import PrinterManager
-from models.Printer import  Printer
-from models.InkjetPrinter import  InkjetPrinter
-from models.LaserPrinter  import  LaserPrinter
-from models.PhotoPrinter  import  PhotoPrinter 
-from models.ThreeDPrinter  import ThreeDPrinter
-
+from managers.manager_printer import PrinterManager
+from models.inkjet_printer import InkjetPrinter
+from models.laser_printer import LaserPrinter
+from models.photo_printer import PhotoPrinter
+from models.three_d_printer import ThreeDPrinter
 
 manager = PrinterManager()
-manager.add_printer(InkjetPrinter("Hp", "inkjet", False, True, 50, 100, "Type1", 100, "Type2", 50, "Type3", 75, "Type4", 80))
-manager.add_printer(InkjetPrinter("Canon", "inkjet", False, True, 55, 100, "Type1", 70, "Type2", 50, "Type3", 105, "Type4", 90))
+manager.add_printer(InkjetPrinter("Hp", "inkjet", False, True, 50, 100, "Type1", 100,
+                                  "Type2", 50, "Type3", 75, "Type4", 80))
+manager.add_printer(InkjetPrinter("Canon", "inkjet", False, True, 55, 100, "Type1", 70,
+                                  "Type2", 50, "Type3", 105, "Type4", 90))
 manager.add_printer(LaserPrinter("Hp", "laser", True, False, 40, 150, 200, 300))
 manager.add_printer(LaserPrinter("Canon", "laser", True, True, 60, 100, 250, 350))
 manager.add_printer(PhotoPrinter("Hp", "inkjet", True, True, 25, 130, 210, 320))
 manager.add_printer(PhotoPrinter("Canon", "laser", True, True, 30, 85, 245, 330))
-manager.add_printer( ThreeDPrinter("Hp", "inkjet", True, True, 45, 70, 220))
+manager.add_printer(ThreeDPrinter("Hp", "inkjet", True, True, 45, 70, 220))
 manager.add_printer(ThreeDPrinter("Canon", "inkjet", True, True, 65, 75, 250))
 
-printers_with_type = manager.find_printer_with_type("laser")
+printers_with_type = manager.find_printer_with_pr_type("laser")
 print("Printers with type 'laser':")
 for printer in printers_with_type:
     print(printer)
@@ -29,5 +29,3 @@ for printer in printers_with_more_paper_than:
     print(printer)
 
 print("\n")
-
-    
